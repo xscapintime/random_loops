632f5a4caac33f325074eb90e002c967af9b28eb
xscapintime <louiesxscape@gmail.com>
2021-09-15 16:43:06 +0800

    zscore vs loop number

--- human/viz/zscore_vs_peak.R
+++ human/viz/zscore_vs_peak.R
@@ -27,10 +27,11 @@ theme_set(theme_few() +
         legend.title=element_blank())
 )
 
+
 ggplot() +
     geom_point(data = dat, aes(x = zscore, y = peak_num, color = zscore),
         position = position_nudge(y = -0.1), size = 2) +
-    scale_color_viridis(option = "viridis") +
+    scale_color_viridis(option = "viridis", limits = c(min(dat$zscore), tail(sort(dat$zscore),2)[1]), oob = scales::squish) +
     geom_text_repel(data = dat %>% filter(abs(zscore) >= 1 | peak_num >= 100000),
                     aes(zscore, peak_num, label = dat %>% filter(abs(zscore) >= 1 | peak_num >= 100000) %>% row.names()),
                 size = 2.8, segment.size = 0.2, segment.color = "#DCDCDC", parse = T) +
@@ -39,4 +40,4 @@ ggplot() +
     stat_cor(data = dat %>% select(zscore, peak_num), aes(zscore, peak_num),
         method = "pearson", label.x.npc = .08, label.y.npc = 1, output.type ="latex")
 
-ggsave("../figs/zscore_vs_peak.pdf", width = 6.7, height = 6.7)
+ggsave("../figs/zscore_vs_peak.png", width = 6.7, height = 6.7)
