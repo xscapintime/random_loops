96af73a368c385d7197be85606b0c2370a3f8560
xscapintime <louiexscape@gmail.com>
2021-10-26 14:57:12 +0800

    add CTCF percentage as size

--- human/viz/histogram_all.R
+++ human/viz/histogram_all.R
@@ -1,4 +1,5 @@
 # histogram for all TF z-score
+# add group spliting line
 # -----------------------------
 
 rm(list = ls())
@@ -18,11 +19,17 @@ smc3 <- dat$zscore[grep("SMC3", row.names(dat))]
 smc1 <- dat$zscore[grep("SMC1A", row.names(dat))]
 
 
+## split into 3 groups
+mk1 <- dat["BCL11A_46178", "zscore"]
+mk2 <- dat["CREB1_46182", "zscore"]
+
+
 ## plot
 library(ggplot2)
+library(ggthemes)
 
 theme_set(
-    theme_classic() +
+    theme_few() +
     theme(legend.position = "none")
 )
 
@@ -31,32 +38,30 @@ p <- ggplot(dat, aes(x = zscore))
 p + geom_histogram(aes(y = stat(density), colour = "black"), fill = "white", bins = 35) +
     geom_density(alpha = 0.4, fill = "#69b3a2") +
     scale_color_manual(values = c("#868686FF")) + ## color of bins
-    geom_vline(aes(xintercept = ctcf_mean),  ## ctcf line
-                linetype = "dashed", size = 0.6, color = "#AE123A") +
-    geom_text(aes(x = ctcf_mean, y = ctcf_mean/2, label = paste0("CTCF: ", round(ctcf_mean, 2))),
-            color = "#AE123A") +
-    geom_vline(aes(xintercept = rad21_mean),  ## rad21 line
-                linetype = "dashed", size = 0.6, color = "#2E5A87") +
-    geom_text(aes(x = rad21_mean, y = rad21_mean/2, label = paste0("RAD21: ", round(rad21_mean, 2))),
-            color = "#2E5A87") +
-    geom_vline(aes(xintercept = smc1),  ## smc1 line
-                linetype = "dashed", size = 0.6, color = "#2E5A87") +
-    geom_text(aes(x = smc1, y = smc1/2, label = paste0("SMC1A: ", round(smc1, 2))),
-            color = "#2E5A87") +
-
-    geom_vline(aes(xintercept = smc3),  ## smc3 line
-                linetype = "dashed", size = 0.6, color = "#2E5A87") +
-    geom_text(aes(x = smc3, y = smc3/2, label = paste0("SMC3: ", round(smc3, 2))),
-            color = "#2E5A87") +
-
+    # geom_vline(aes(xintercept = ctcf_mean),  ## ctcf line
+                # linetype = "dotted", size = 0.6, color = "#AE123A") +
+    geom_text(aes(x = ctcf_mean, y = .01, label = paste0("CTCF: ", round(ctcf_mean, 2))), color = "#AE123A", angle = 45, size = 3, nudge_x = .15) +
+    # geom_vline(aes(xintercept = rad21_mean),  ## rad21 line
+                # linetype = "dotted", size = 0.6, color = "#2E5A87") +
+    geom_text(aes(x = rad21_mean, y = .01, label = paste0("RAD21: ", round(rad21_mean, 2))), color = "#2E5A87", angle = 45, size = 3) +
+    # geom_vline(aes(xintercept = smc1),  ## smc1 line
+                # linetype = "dotted", size = 0.6, color = "#2E5A87") +
+    geom_text(aes(x = smc1, y = .01, label = paste0("SMC1A: ", round(smc1, 2))), color = "#2E5A87", angle = 45, size = 3) +
+    # geom_vline(aes(xintercept = smc3),  ## smc3 line
+                # linetype = "dotted", size = 0.6, color = "#2E5A87") +
+    geom_text(aes(x = smc3, y = .01, label = paste0("SMC3: ", round(smc3, 2))), color = "#2E5A87", angle = 45, size = 3) +
+    geom_vline(aes(xintercept = mk1),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
+    geom_vline(aes(xintercept = mk2),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
     # geom_vline(aes(xintercept = wapl),  ## wapl line
             #   linetype = "dashed", size = 0.8, color = "#5D6D7E")+
     # geom_text(aes(x = wapl, y = 0.5, label = paste0("WAPL: ", round(wapl, 2))),
             #   color = "#34495E")+
 
-    labs(title = "Loop Z-Score Histogram",
-        subtitle = "Human") +
+    # labs(title = "Loop Z-Score Histogram", subtitle = "Human") +
     ylab("Dens.") +
     xlab("Z-Score") +
     ylim(0, 0.85)
-ggsave(filename = "hisogram_all_zscore.png", path = "../figs/", width = 7, height = 7)
+ggsave(filename = "hisogram_all_zscore.png", path = "../figs/", width = 6.7, height = 6.7)
+ggsave(filename = "hisogram_all_zscore.pdf", path = "../figs/", width = 6.7, height = 6.7)
--- human/viz/zscore_vs_peak.R
+++ human/viz/zscore_vs_peak.R
@@ -2,12 +2,26 @@
 # -----------------------------
 
 rm(list = ls())
-options(scipen = 999)
+# options(scipen = 999)
 
 library(tidyverse)
 
 ## load table
-dat <- read.csv("../looping_score/looping_score.fin.txt", sep = "\t", header = T, row.names = 1)
+# loop zscore
+zscore <- read.csv("../looping_score/looping_score.fin.txt", sep = "\t", header = T, row.names = 1)
+
+# peak percentage against CTCF
+per <- read.csv("../../../peak_overlap/hs/getpct/ave_ind_pct.txt",  sep = "\t", header = T, row.names = 1)
+colnames(per) <- "percent"
+
+## merge
+dat <- merge(zscore, per * 100, by = 0, all = TRUE)
+dat <- arrange(dat, desc(zscore))
+
+
+## split into 3 groups
+mk1 <- (dat %>% filter(Row.names == "BCL11A_46178"))$zscore
+mk2 <- (dat %>% filter(Row.names == "CREB1_46182"))$zscore
 
 
 ## plot
@@ -19,25 +33,57 @@ library(ggpubr)
 
 
 theme_set(theme_few() +
-    theme(legend.justification = c(0, 1),
-        legend.position = c(0, 1),
+    theme(
+        legend.justification = c("right", "bottom"),
+        legend.position = c(1, .02),
         legend.background = element_blank(),
         legend.key = element_blank(),
-        legend.key.size = unit(14, "pt"),
-        legend.title=element_blank())
+        legend.key.size = unit(.35, "cm"),
+        legend.title = element_text(size = 10))
+        # legend.text  = element_text(size = 8))
+        # legend.title = element_blank())
 )
 
 
 ggplot() +
-    geom_point(data = dat, aes(x = zscore, y = peak_num, color = zscore),
-        position = position_nudge(y = -0.1), size = 2) +
-    scale_color_viridis(option = "viridis", limits = c(min(dat$zscore), tail(sort(dat$zscore),2)[1]), oob = scales::squish) +
+    geom_point(data = dat, aes(x = zscore, y = peak_num, fill = zscore, size = percent),
+        position = position_nudge(y = -0.1), shape = 21, alpha = .8) +
+    scale_fill_viridis(option = "viridis", limits = c(min(dat$zscore), tail(sort(dat$zscore),2)[1]), oob = scales::squish) +
     geom_text_repel(data = dat %>% filter(abs(zscore) >= 1 | peak_num >= 100000),
-                    aes(zscore, peak_num, label = dat %>% filter(abs(zscore) >= 1 | peak_num >= 100000) %>% row.names()),
+                    aes(zscore, peak_num, label = Row.names),
                 size = 2.8, segment.size = 0.2, segment.color = "#DCDCDC", parse = T) +
-    scale_fill_gradient(limits = c(min(dat$zscore), max(dat$zscore))) +
-    xlab("Looping Z-score") + ylab("Peak number") +
+    # scale_fill_gradient(limits = c(min(dat$zscore), max(dat$zscore))) +
+    geom_vline(aes(xintercept = mk1),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
+    geom_vline(aes(xintercept = mk2),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
+    xlab("Loop Z-score") + ylab("Peak Num.") +
+    labs(fill = "Loop Z-score", size = "% CTCF Peaks") +
     stat_cor(data = dat %>% select(zscore, peak_num), aes(zscore, peak_num),
-        method = "pearson", label.x.npc = .08, label.y.npc = 1, output.type ="latex")
+        method = "pearson", label.x.npc = 0, label.y.npc = 1, output.type ="latex")
 
 ggsave("../figs/zscore_vs_peak.png", width = 6.7, height = 6.7)
+ggsave("../figs/zscore_vs_peak.pdf", width = 6.7, height = 6.7)
+
+
+
+## peak num vs zscore
+ggplot() +
+    geom_point(data = dat, aes(x = peak_num, y = zscore, fill = zscore, size = percent),
+        position = position_nudge(y = -0.1), shape = 21, alpha = .8) +
+    scale_fill_viridis(option = "viridis", limits = c(min(dat$zscore), tail(sort(dat$zscore),2)[1]), oob = scales::squish) +
+    geom_text_repel(data = dat %>% filter(abs(zscore) >= 1 | peak_num >= 100000),
+                    aes(peak_num, zscore, label = Row.names),
+                size = 2.8, segment.size = 0.2, segment.color = "#DCDCDC", parse = T) +
+    # scale_fill_gradient(limits = c(min(dat$zscore), max(dat$zscore))) +
+    geom_hline(aes(yintercept = mk1),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
+    geom_hline(aes(yintercept = mk2),
+                linetype = "dotted", size = 0.6, color = "#5D6D7E") +
+    ylab("Loop Z-score") + xlab("Peak Num.") +
+    labs(fill = "Loop Z-score", size = "% CTCF Peaks") +
+    stat_cor(data = dat %>% select(peak_num, zscore), aes(peak_num, zscore),
+        method = "pearson", label.x.npc = 0, label.y.npc = 1, output.type ="latex")
+
+ggsave("../figs/peak_vs_zscore.png", width = 6.7, height = 6.7)
+ggsave("../figs/peak_vs_zscore.pdf", width = 6.7, height = 6.7)
