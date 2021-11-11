d84beafbac3592f055eaf7ceac5e336935eb8598
xscapintime <louiexscape@gmail.com>
2021-11-11 15:21:17 +0800

    add ENCODE ChIP-seq data

---
+++ human/data/encode_bed/upload_data.sh
@@ -0,0 +1,1 @@
+rsync -avP *.bed.gz liyang@172.18.4.20:/home/liyang/data3/encode_gl/bed
---
+++ mouse/data/encode_bed/wget_encode.sh
@@ -0,0 +1,5 @@
+wget -c https://www.encodeproject.org/files/ENCFF927JDA/@@download/ENCFF927JDA.bed.gz -O CHD2_ENCFF927JDA.bed.gz
+wget -c https://www.encodeproject.org/files/ENCFF058DYG/@@download/ENCFF058DYG.bed.gz -O HCFC1_ENCFF058DYG.bed.gz
+wget -c https://www.encodeproject.org/files/ENCFF496JHC/@@download/ENCFF496JHC.bed.gz -O MAFK_ENCFF496JHC.bed.gz
+wget -c https://www.encodeproject.org/files/ENCFF404QDD/@@download/ENCFF404QDD.bed.gz -O ZC3H11A_ENCFF404QDD.bed.gz
+wget -c https://www.encodeproject.org/files/ENCFF361ESB/@@download/ENCFF361ESB.bed.gz -O ZNF384_ENCFF361ESB.bed.gz
