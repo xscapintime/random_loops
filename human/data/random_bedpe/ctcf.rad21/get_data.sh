936c563a9546ea5b953eb84d0d87cf591bd7079e
xscapintime <louiexscape@gmail.com>
2021-11-17 17:41:45 +0800

    random contact files

---
+++ human/data/random_bedpe/ctcf.rad21/get_data.sh
@@ -0,0 +1,1 @@
+ rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/ctcf.rad21/CTCF_36632*.bed' .
---
+++ human/data/random_bedpe/get_data.sh
@@ -0,0 +1,5 @@
+rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/NANOG*.bed' .
+rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/MAFK*.bed' .
+rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/MAX*.bed' .
+rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/EZH2*.bed' .
+rsync -avP 'liyang@172.18.4.20://home/liyang/data3/random_loops/human/getloops/KDM4A*.bed' .
